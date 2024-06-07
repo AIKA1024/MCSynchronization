f5ca1ec990b3ac0a902f428d90fb206b6dea8e57
AIKA <1343707921@qq.com>
2024-06-07 20:21:17 +0800

    更新版本

--- 马自达MC同步器/bulid.ps1
+++ 马自达MC同步器/bulid.ps1
@@ -1,1 +1,1 @@
-vpk pack --framework net8.0-x64-desktop -u 马自达MC同步器 -v 0.0.3  -p .\bin\Release\net8.0-windows -e 马自达MC同步器.exe
+vpk pack --framework net8.0-x64-desktop -u MAZDAMCTools -v 0.0.4  -p .\bin\Release\net8.0-windows -e 马自达MC同步器.exe
