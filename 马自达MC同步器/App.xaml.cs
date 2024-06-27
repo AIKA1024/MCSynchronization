205129aae5c77dbd24c08658f3b460948f6bf67c
AIKA <1343707921@qq.com>
2024-06-27 19:49:02 +0800

    发布版本的缓存修改为上一级

--- 马自达MC同步器/App.xaml.cs
+++ 马自达MC同步器/App.xaml.cs
@@ -16,9 +16,15 @@ public partial class App : Application
 
   public WebHelper webHelper = new();
 
+#if DEBUG
   public readonly string CachePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Cache");
   public readonly string LogoPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Cache","Logo");
   public readonly string DownloadPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Cache", "Download");
+#else
+  public readonly string CachePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\Cache");
+  public readonly string LogoPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\Cache","Logo");
+  public readonly string DownloadPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\Cache", "Download");
+#endif
   public string ModPath => Path.Combine(Settings.Default.GamePath, "mods");
   public new static App Current => (App)Application.Current;
 
